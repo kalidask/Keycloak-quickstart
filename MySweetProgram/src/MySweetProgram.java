d0408a493695acf6185565e0e0140ce9262e3b32
kalidas <kalidas@redhat.com>
2020-12-20 14:12:36 +0530

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("What's your name ?");
+
+		
+		/* Scanner in = new Scanner(System.in);
+
+		String s = in.nextLine();
+		
+		if(s.equals("kalidas")) {
+			System.out.println("Hey Kali!");
+			
+		} else if (s.equals("Pradeep")) {
+			System.out.println("Hey paddy!");
+		} else {
+			System.out.println("You are not welcome here!");
+		}
+
+		in.close();
+		//System.out.println(s);
+		 	int i=0;
+		while (i < 10) {
+			System.out.println("My i value is :"+i);
+			i++;
+		}
+		*/
+		
+		
+		for (int i =0; i < 10; i++) {
+			System.out.println("My i value is :"+i);
+
+		}
+		
+		
+	
+	}
+
+}
